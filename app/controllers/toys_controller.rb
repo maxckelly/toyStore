ae17af6ce73172221ffd53f75fdc7cf222c32d53
Max Kelly <max@revelthat.com>
2019-10-15 16:56:50 +1100

    made changes

---
+++ app/controllers/toys_controller.rb
@@ -0,0 +1,13 @@
+class ToysController < ApplicationController
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/", to: "toys#index", as: "root"
+  get "/toy_create", to: "toys#create"
 end
