be854f99fa19ce1aa0bc65ee46c03a29cf2be707
Max Kelly <max@revelthat.com>
2019-10-15 14:52:54 +1100

    created a seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+for toy in 1..100
+  Toy.create(
+    name: Faker::Movies::StarWars.specie,
+    description: Faker::Quote.yoda,
+    date: Faker::Date.between(from: 200.days.ago, to: Date.today),
+    user: Faker::Name.name
+  )
+
+  puts "Created #{toy} records"
+end
